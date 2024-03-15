016b600db7eadb9c4ad9a86ed8615d3788e99d73
loweffort-alt <farfan_alexander@outlook.com>
2024-03-15 13:49:46 -0500

    Hotfix: Change modals logic

--- src/components/Modals/ProjectModal/ProjectModal.jsx
+++ src/components/Modals/ProjectModal/ProjectModal.jsx
@@ -1,33 +1,45 @@
-import React from 'react';
-import { createPortal } from 'react-dom';
+import React from "react";
+import { createPortal } from "react-dom";
 
-import { getImageUrl } from '../../../utils';
+import { getImageUrl } from "../../../utils";
 
-import styles from './ProjectModal.module.css';
+import styles from "./ProjectModal.module.css";
 
-const mountElement = document.getElementById('project-modal');
+const mountElement = document.getElementById("project-modal");
 
-export const ProjectModal = ( {isOpened, onClose, children} ) => {
-    if ( !isOpened ) {
-        return null;
-    }
-    return (
-    createPortal(
-        <div className={styles.modal}>
-            {/* <div className={styles.overlay}></div> */}
-            <div className={styles.container}>
-                {/* <span className={styles.closeBtn} onClick={onClose}>X</span> */}
-                <img 
-                className={styles.closeBtn} 
-                src={getImageUrl('nav/closeMenu.svg')} 
-                onClick={onClose}
-                alt="menu-button"
-                />
-                <div className={styles.children}>
-                    {children}
-                </div>
+export const ProjectModal = ({ isOpened, onClose, data }) => {
+  if (!isOpened) {
+    return null;
+  }
+
+  return createPortal(
+    <div className={styles.modal}>
+      {/* <div className={styles.overlay}></div> */}
+      <div className={styles.container}>
+        {/* <span className={styles.closeBtn} onClick={onClose}>X</span> */}
+        <img
+          className={styles.closeBtn}
+          src={getImageUrl("nav/closeMenu.svg")}
+          onClick={onClose}
+          alt="menu-button"
+        />
+        <div className={styles.children}>
+          <div className="abc">
+            <img src="https://picsum.photos/500/500" alt="image" />
+            <div>
+              <h2>{data[0]}</h2>
+              <p>{data[1]}</p>
+              <p>
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                Blanditiis distinctio, quos alias dolorum quam, ab omnis,
+                laboriosam nemo voluptas fuga itaque nisi odio expedita hic
+                doloremque eum delectus dolor? Recusandae.
+              </p>
             </div>
+          </div>
         </div>
-    , mountElement)
-  )
-}
+      </div>
+    </div>,
+    mountElement,
+  );
+};
--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,43 +1,54 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 
-import { ProjectModal } from '../Modals/ProjectModal/ProjectModal';
+import { ProjectModal } from "../Modals/ProjectModal/ProjectModal";
 
-import { getImageUrl } from '../../utils';
+import { getImageUrl } from "../../utils";
 
-import styles from './ProjectCard.module.css';
+import styles from "./ProjectCard.module.css";
 
-export const ProjectCard = ({project: {title, imageSrc, description, skills, demo, source}}) => {
-    const [isModalOpened, setisModalOpened] = useState(false);
-    console.log('isModalOpened', isModalOpened);
+export const ProjectCard = ({
+  project: { title, imageSrc, description, skills, demo, source },
+}) => {
+  const [isModalOpened, setisModalOpened] = useState(false);
+  console.log("isModalOpened", isModalOpened);
 
-    return (
-    <div className={styles.container}>
-        <img className={styles.image} src={getImageUrl(imageSrc)} alt={`Image of ${title}`} />
-        <h3 className={styles.title}>{title}</h3>
-        <p className={styles.description}>{description}</p>
-        <ul className={styles.skills}>
-            {
-                skills.map( (skill, id) => {
-                    return <li className={styles.skill} key={id}>{skill}</li>
-                } )
-            }
-        </ul>
-        <div className={styles.links}>
-            <a className={styles.link} href={demo}>Demo</a>
-            <button onClick={ () => setisModalOpened(true) }>Sample</button>
-            <ProjectModal className={styles.projectModal} isOpened={isModalOpened} onClose={ () => setisModalOpened(false) }>
-                <div class="abc">
-                    <img src='https://picsum.photos/500/500' alt="image" />
-                    <div>
-                        <h2>{title}</h2>
-                        <p>{description}</p>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis distinctio, quos alias dolorum quam, ab omnis, laboriosam nemo voluptas fuga itaque nisi odio expedita hic doloremque eum delectus dolor? Recusandae.</p>
-                    </div>
-                </div>
-            </ProjectModal>
-            <a className={styles.link} href={source}>Source</a>
-        </div>
+  return (
+    <div
+      className={styles.container}
+      onClick={() => setisModalOpened(!isModalOpened)}
+    >
+      <img
+        className={styles.image}
+        src={getImageUrl(imageSrc)}
+        alt={`Image of ${title}`}
+      />
+      <h3 className={styles.title}>{title}</h3>
+      <p className={styles.description}>{description}</p>
+      <ul className={styles.skills}>
+        {skills.map((skill, id) => {
+          return (
+            <li className={styles.skill} key={id}>
+              {skill}
+            </li>
+          );
+        })}
+      </ul>
+      <div className={styles.links}>
+        <a className={styles.link} href={demo}>
+          Demo
+        </a>
+        {/* <button onClick={() => setisModalOpened(true)}>Sample</button> */}
+        <ProjectModal
+          className={styles.projectModal}
+          isOpened={isModalOpened}
+          onClose={() => setisModalOpened(false)}
+          data={[title, description]}
+        />
+        <a className={styles.link} href={source}>
+          Source
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
