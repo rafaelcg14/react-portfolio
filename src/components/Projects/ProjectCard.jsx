b09fc87be40371b995dc9328f3eab8cc0f1103c6
StarLordCG <rafaellord_123@outlook.com>
2024-03-16 15:11:22 -0500

    Modals working

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -7,37 +7,36 @@ import { getImageUrl } from '../../utils';
 
 import styles from './ProjectCard.module.css';
 
-export const ProjectCard = ({project: {title, imageSrc, description, skills, demo, source}}) => {
+export const ProjectCard = ({project: {title, imageSrc, description, skills, demo, source, tools, images}}) => {
     const [isModalOpened, setisModalOpened] = useState(false);
     console.log('isModalOpened', isModalOpened);
 
     return (
-    <div className={styles.container}>
-        <img className={styles.image} src={getImageUrl(imageSrc)} alt={`Image of ${title}`} />
-        <h3 className={styles.title}>{title}</h3>
-        <p className={styles.description}>{description}</p>
-        <ul className={styles.skills}>
-            {
-                skills.map( (skill, id) => {
-                    return <li className={styles.skill} key={id}>{skill}</li>
-                } )
-            }
-        </ul>
-        <div className={styles.links}>
-            <a className={styles.link} href={demo}>Demo</a>
-            <button onClick={ () => setisModalOpened(true) }>Sample</button>
-            <ProjectModal className={styles.projectModal} isOpened={isModalOpened} onClose={ () => setisModalOpened(false) }>
-                <div class="abc">
-                    <img src='https://picsum.photos/500/500' alt="image" />
-                    <div>
-                        <h2>{title}</h2>
-                        <p>{description}</p>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis distinctio, quos alias dolorum quam, ab omnis, laboriosam nemo voluptas fuga itaque nisi odio expedita hic doloremque eum delectus dolor? Recusandae.</p>
-                    </div>
+        <div>
+            <ProjectModal 
+                className={styles.projectModal} 
+                isOpened={isModalOpened} 
+                onClose={ () => setisModalOpened(false) }
+                details={{title, description, skills, tools, images}}
+            />
+            <div 
+                className={styles.container}
+                onClick={ () => setisModalOpened(!isModalOpened) }
+            >
+                <img className={styles.image} src={getImageUrl(imageSrc)} alt={`Image of ${title}`} />
+                <h3 className={styles.title}>{title}</h3>
+                <p className={styles.description}>{description}</p>
+                <ul className={styles.skills}>
+                    {
+                        skills.map( (skill, id) => {
+                            return <li className={styles.skill} key={id}>{skill}</li>
+                        } )
+                    }
+                </ul>
+                <div className={styles.links}>
+                    <a className={styles.link}>See details</a>
                 </div>
-            </ProjectModal>
-            <a className={styles.link} href={source}>Source</a>
+            </div>
         </div>
-    </div>
   )
 }
