19b624986faa2d7e616e6c99ba2d4abb84ea41a0
StarLordCG <rafaellord_123@outlook.com>
2024-05-10 09:34:48 -0500

    change of directory for cv file

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,6 +3,8 @@ import skillsData from '../../data/skillsData.json';
 import styles from './About.module.css';
 import { CardAbout } from '../CardAbout/CardAbout';
 
+import cv from '../../static/cv.pdf';
+
 export const About = () => {
 
   return (
@@ -15,7 +17,8 @@ export const About = () => {
               <h4 className={styles.name}>Hans Díaz</h4>
               <span className={styles.tagName}>3D Artist | Modeling and Texturing</span>
               <p className={styles.description}>Hans Díaz, Artista 3D y egresado de Toulouse Lautrec, destaco en técnicas 3D y VFX para cine, publicidad y videojuegos. Experto en modelado y texturizado en Maya, Blender, ZBrush, Substance Painter, Marmoset Toolbag y Arnold Renderer. Trabajé en Star Toons Animation modelando y texturizando para el cortometraje “Un Vals Así”. Apasionado por crear y dar vida a escenarios y personajes 3D, es meticuloso, detallista y valora el trabajo en equipo, la comunicación asertiva y el liderazgo.</p>
-              <a className={styles.resumeBtn} href="../../../public/cv.pdf" download="Hans_Diaz_3D_Artist_CV.pdf">Download CV</a>
+              {/* <a className={styles.resumeBtn} href="../../../public/cv.pdf" download="Hans_Diaz_3D_Artist_CV.pdf">Download CV</a> */}
+              <a className={styles.resumeBtn} href={cv} download="Hans_Diaz_3D_Artist_CV.pdf">Download CV</a>
             </div>
             <div className={styles.aboutCard}>
               <img className={styles.heroImg} src={getImageUrl('hero/hero.jpg')} alt="Hero image of me" />
